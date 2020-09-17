a72c24af963ef7c49a668998d8cf277565b96154
Aswin Sampath <aswinsampath.v@gmail.com>
2020-09-17 17:10:41 +0530

    final submission

---
+++ edited/SALOKYA/js/read more.js
@@ -0,0 +1,12 @@
+<!-- Read more script -->
+<script>
+function readless(elem) {
+var btnTxt = document.getElementById(elem.id);
+if(btnTxt.innerHTML=="READ MORE"){
+btnTxt.innerHTML = "READ LESS";
+}
+else{
+btnTxt.innerHTML = "READ MORE";
+}
+}
+</script>
